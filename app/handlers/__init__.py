97bfd5c43d46cd83c16b2542b661ea54772a0fba
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 08:08:45 +0300

    add order router

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -3,7 +3,12 @@ __all__ = ("router",)
 from aiogram import Router
 
 from handlers import other_message
+from handlers import order
 
 router = Router(name=__name__)
 
+router.include_router(order.router)
+
+
+# должен быть последним
 router.include_router(other_message.router)
