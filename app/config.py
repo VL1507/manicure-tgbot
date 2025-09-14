06a39363d40dc643e72ab096063313a8b6f1382a
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 06:06:33 +0300

    add class for bot and db

--- app/config.py
+++ app/config.py
@@ -1,8 +1,24 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class BOT(BaseModel):
+    TOKEN: str
+
+
+class DB(BaseModel):
+    # USER: str
+    # PASSWORD: str
+    # HOST: str
+    # PORT: int
+    # NAME: str
+
+    URL: str
+
+
 class Settings(BaseSettings):
-    BOT_TOKEN: str
+    BOT: BOT
+    DB: DB
 
     model_config = SettingsConfigDict(env_file="./.env", env_nested_delimiter="__")
 
