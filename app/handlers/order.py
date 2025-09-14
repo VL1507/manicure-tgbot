6d550a6b91035aea0deaaa30bb374061d11e4864
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 17:13:20 +0300

    add choice of days

--- app/handlers/order.py
+++ app/handlers/order.py
@@ -3,7 +3,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import CallbackQuery
 
-from infrastructure.database.requests import get_services
+from infrastructure.database.requests import get_services, get_service_by_id
 from keyboards import kb
 
 
@@ -28,28 +28,64 @@ async def order_callback_handler(callback: CallbackQuery, state: FSMContext):
 
     await state.set_state(Order.service)
 
-    selected_services = []
-    await state.update_data({"selected_services": selected_services})
+    selected_services_id = []
+    await state.update_data({"selected_services_id": selected_services_id})
 
 
 @router.callback_query(F.data.startswith("service_"), Order.service)
 async def service_callback_handler(callback: CallbackQuery, state: FSMContext):
     service_id = int(callback.data.split("_")[1])
 
-    selected_services: list[int] = await state.get_value("selected_services")
+    selected_services_id: list[int] = await state.get_value("selected_services_id")
 
-    selected_services.append(service_id)
+    selected_services_id.append(service_id)
 
-    await state.update_data({"selected_services": selected_services})
+    await state.update_data({"selected_services_id": selected_services_id})
 
     all_services = await get_services()
-    not_selected_services = [s for s in all_services if s.id not in selected_services]
+    not_selected_services = [
+        s for s in all_services if s.id not in selected_services_id
+    ]
 
     text = (
         "Выбрана(ы) услуга(и):\n"
-        + "\n".join([s.name for s in all_services if s.id in selected_services])
+        + "\n".join([s.name for s in all_services if s.id in selected_services_id])
         + "\n\nЖелаете дополнительные услуги?"
     )
-    await callback.message.answer(
-        text, reply_markup=kb.service_keyboard(not_selected_services)
+    try:
+        await callback.message.edit_text(
+            text, reply_markup=kb.service_keyboard(not_selected_services)
+        )
+    except Exception as e:
+        await callback.message.answer(
+            text, reply_markup=kb.service_keyboard(not_selected_services)
+        )
+
+
+@router.callback_query(F.data == "next_from_service", Order.service)
+async def next_from_service_handler(callback: CallbackQuery, state: FSMContext):
+    selected_services_id: list[int] = await state.get_value("selected_services_id")
+
+    if len(selected_services_id) == 0:
+        await callback.answer("Необходимо выбрать услугу(и)", show_alert=True)
+        return
+
+    await state.set_state(Order.day)
+
+    selected_services = [await get_service_by_id(sid) for sid in selected_services_id]
+
+    await state.update_data({"selected_services": selected_services})
+
+    text = (
+        "На какой день запишемся на услугу(и): "
+        + ", ".join([s.name for s in selected_services if s is not None])
+        + "?"
     )
+
+    work_duration = sum([s.duration_minutes for s in selected_services])
+
+    await callback.message.answer(
+        text,
+        reply_markup=await kb.days_keyboard(offset_days=0, work_duration=work_duration),
+    )
+    await callback.message.delete()
