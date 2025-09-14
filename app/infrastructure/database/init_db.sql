d458f186ee651878a763b4cec0a5fff7a2f11ed3
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 08:08:25 +0300

    sql to fill services table

---
+++ app/infrastructure/database/init_db.sql
@@ -0,0 +1,8 @@
+-- database: ../../../manicure.db
+INSERT INTO
+    services (name, duration_minutes)
+VALUES
+    ('Маникюр', 70),
+    ('Педикюр', 120),
+    ('Наращивание', 40),
+    ('Все включено', 230);
