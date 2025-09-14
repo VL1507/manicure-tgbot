deb3fbca4729a472532088e50e2f6a6e0d801ec1
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 07:25:07 +0300

    remove link_preview_is_disabled

--- app/main.py
+++ app/main.py
@@ -18,9 +18,7 @@ async def main():
 
     bot = Bot(
         token=settings.BOT.TOKEN,
-        default=DefaultBotProperties(
-            parse_mode=ParseMode.HTML, link_preview_is_disabled=True
-        ),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
 
     await bot.delete_webhook(drop_pending_updates=True)
