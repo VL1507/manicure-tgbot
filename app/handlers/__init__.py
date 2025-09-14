7dc708864186de06bcdb618bf3fba72ec6bba617
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 12:24:48 +0300

    add cancel, start

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,11 +2,11 @@ __all__ = ("router",)
 
 from aiogram import Router
 
-from handlers import order, other_message
+from handlers import order, other_message, cancel, start
 
 router = Router(name=__name__)
 
-router.include_router(order.router)
+router.include_routers(order.router, cancel.router, start.router)
 
 
 # должен быть последним
