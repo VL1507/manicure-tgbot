db944f0f96d0ff7ef5c8a56d7de2e904104f5b0f
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 05:35:45 +0300

    moved dp and bot in main

--- Telegram.py
+++ Telegram.py
@@ -13,18 +13,20 @@ load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
 
 
-dp = Dispatcher()
-dp.include_routers(commands.router_commands, new_order.router_new_order)
-
-
-Bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML,
-                                                    link_preview_is_disabled=True))
-
-
 async def main():
+    dp = Dispatcher()
+    dp.include_routers(commands.router_commands, new_order.router_new_order)
+
+    bot = Bot(
+        token=TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML, link_preview_is_disabled=True
+        ),
+    )
+
     await Bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(Bot)
+    await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
