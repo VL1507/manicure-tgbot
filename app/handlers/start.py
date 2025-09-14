62534128ce901988f276725b41475c65083c6e10
VL1507 <vova.mashyanov@gmail.com>
2025-09-14 12:25:11 +0300

    create start handler

---
+++ app/handlers/start.py
@@ -0,0 +1,19 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+
+from keyboards import kb
+
+
+router = Router(name=__name__)
+
+
+@router.message(CommandStart())
+async def start_command_handler(message: Message, state: FSMContext):
+    await state.clear()
+
+    await message.answer(
+        "Привет! Это бот для записи к мастеру салона красоты 'Мадина'!",
+        reply_markup=kb.start,
+    )
