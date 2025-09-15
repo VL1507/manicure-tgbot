f1dd581a64867bf6f58509c7a1faa674b81a8cb3
VL1507 <vova.mashyanov@gmail.com>
2025-09-15 14:15:28 +0300

    create save_appointments

--- app/infrastructure/database/requests.py
+++ app/infrastructure/database/requests.py
@@ -1,6 +1,6 @@
 from sqlalchemy import select
 
-from datetime import datetime, date, time
+import datetime as dt
 
 from infrastructure.database.accessor import get_db_session
 from infrastructure.database.models import Appointments, Services, ServiceToAppointments
@@ -21,7 +21,9 @@ async def get_service_by_id(service_id: int) -> Services | None:
         return service.scalar()
 
 
-async def get_available_slots(date: date, work_duration: int) -> list[tuple[str, str]]:
+async def get_available_slots(
+    date: dt.date, work_duration: int
+) -> list[tuple[str, str]]:
     async with get_db_session() as session:
         stmt = select(Appointments).where(Appointments.date == date)
         result = await session.execute(stmt)
@@ -29,8 +31,8 @@ async def get_available_slots(date: date, work_duration: int) -> list[tuple[str,
 
         booked_intervals = []
         for appointment in appointments:
-            start_time = datetime.strptime(appointment.time_start, "%H:%M").time()
-            end_time = datetime.strptime(appointment.time_end, "%H:%M").time()
+            start_time = dt.datetime.strptime(appointment.time_start, "%H:%M").time()
+            end_time = dt.datetime.strptime(appointment.time_end, "%H:%M").time()
 
             start_time_minutes = start_time.hour * 60 + start_time.minute
             end_time_minutes = end_time.hour * 60 + end_time.minute
@@ -39,11 +41,12 @@ async def get_available_slots(date: date, work_duration: int) -> list[tuple[str,
 
         available_slots: list[tuple[str, str]] = []
 
-        if date != datetime.now().date():
+        if date != dt.datetime.now().date():
             work_start_minutes = WORK_TIME_START * 60
         else:
             work_start_minutes = max(
-                WORK_TIME_START * 60, datetime.now().hour * 60 + datetime.now().minute
+                WORK_TIME_START * 60,
+                dt.datetime.now().hour * 60 + dt.datetime.now().minute,
             )
             work_start_minutes = (
                 work_start_minutes // SLOT_DURATION + 1
@@ -66,10 +69,35 @@ async def get_available_slots(date: date, work_duration: int) -> list[tuple[str,
                     break
 
             if is_available:
-                start_time = time(start_minutes // 60, start_minutes % 60)
-                end_time = time(end_minutes // 60, end_minutes % 60)
+                start_time = dt.time(start_minutes // 60, start_minutes % 60)
+                end_time = dt.time(end_minutes // 60, end_minutes % 60)
                 available_slots.append(
                     (start_time.strftime("%H:%M"), end_time.strftime("%H:%M"))
                 )
 
         return available_slots
+
+
+async def save_appointments(
+    user_id: int,
+    date: dt.date,
+    time_start: str,
+    time_end: str,
+    selected_services_id: list[int],
+):
+    async with get_db_session() as session:
+        appointment = Appointments(
+            user_id=user_id, date=date, time_start=time_start, time_end=time_end
+        )
+
+        session.add(appointment)
+
+        await session.commit()
+        await session.refresh(appointment)
+
+        for service_id in selected_services_id:
+            service_to_appointment = ServiceToAppointments(
+                service_id=service_id, appointment_id=appointment.id
+            )
+            session.add(service_to_appointment)
+        await session.commit()
