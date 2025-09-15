9ac98397bedfaa4c54d23952d11f827df88399af
VL1507 <vova.mashyanov@gmail.com>
2025-09-15 13:57:18 +0300

    fix time bug

--- app/infrastructure/database/requests.py
+++ app/infrastructure/database/requests.py
@@ -38,9 +38,17 @@ async def get_available_slots(date: date, work_duration: int) -> list[tuple[str,
             booked_intervals.append((start_time_minutes, end_time_minutes))
 
         available_slots: list[tuple[str, str]] = []
-        work_start_minutes = max(
-            WORK_TIME_START * 60, datetime.now().hour * 60 + datetime.now().minute
-        )
+
+        if date != datetime.now().date():
+            work_start_minutes = WORK_TIME_START * 60
+        else:
+            work_start_minutes = max(
+                WORK_TIME_START * 60, datetime.now().hour * 60 + datetime.now().minute
+            )
+            work_start_minutes = (
+                work_start_minutes // SLOT_DURATION + 1
+            ) * SLOT_DURATION
+
         work_end_minutes = WORK_TIME_END * 60
 
         for start_minutes in range(
