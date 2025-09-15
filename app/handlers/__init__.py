b725078edde1a33fa80826d2e78ec3b52cf660bb
VL1507 <vova.mashyanov@gmail.com>
2025-09-15 06:32:49 +0300

    func for back_from_date

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -2,11 +2,13 @@ __all__ = ("router",)
 
 from aiogram import Router
 
-from handlers import order, other_message, cancel, start, choice_day
+from handlers import order, other_message, cancel, start, choice_day, back_from_date
 
 router = Router(name=__name__)
 
-router.include_routers(order.router, cancel.router, start.router, choice_day.router)
+router.include_routers(
+    order.router, cancel.router, start.router, choice_day.router, back_from_date.router
+)
 
 
 # должен быть последним
--- app/handlers/back_from_date.py
+++ app/handlers/back_from_date.py
@@ -2,38 +2,18 @@ from aiogram import F, Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
 
-from infrastructure.database.requests import get_services, get_service_by_id
+from infrastructure.database.requests import get_services
 from keyboards import kb
 from states.order import Order
 
 router = Router(name=__name__)
 
 
-@router.callback_query(F.data == "order")
-async def order_callback_handler(callback: CallbackQuery, state: FSMContext):
-    all_services = await get_services()
-
-    await callback.message.answer(
-        "Выберите желаемую услугу:", reply_markup=kb.service_keyboard(all_services)
-    )
-
+@router.callback_query(F.data == "back_from_date", Order.day)
+async def back_from_date(callback: CallbackQuery, state: FSMContext):
     await state.set_state(Order.service)
-
-    selected_services_id = []
-    await state.update_data({"selected_services_id": selected_services_id})
-
-
-@router.callback_query(F.data.startswith("service_"), Order.service)
-async def service_callback_handler(callback: CallbackQuery, state: FSMContext):
-    await state.set_state(Order.service)
-
-    service_id = int(callback.data.split("_")[1])
 
     selected_services_id: list[int] = await state.get_value("selected_services_id")
-
-    selected_services_id.append(service_id)
-
-    await state.update_data({"selected_services_id": selected_services_id})
 
     all_services = await get_services()
     not_selected_services = [
